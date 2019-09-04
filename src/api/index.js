664a5c208b88dbd1852499b657096dde6b480e2a
gays <1292984232@qq.com>
2019-09-04 20:38:34 +0800

    api

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+import music from "./music";
+import uset from "./user";
+
+export default{
+    user,
+    music
+}
---
+++ src/api/user.js
@@ -0,0 +1,10 @@
+import request from "@/utils/request"
+
+let user ={
+    login:data=>request.post("/api/login",data),
+    registry:data=>request.post('/api/registry',data),
+    getInfo:()=>request.get("/api/userinfo")
+}
+
+
+export default user
